82fb5c41f8e1d126e631a1a9ab495c000df4bc2c
Pablo Fonseca <pablo.fonsecam@outlook.com>
2020-07-29 11:25:22 -0600

    Fonseca_M_Pablo: First Atoms Implemented

---
+++ src/views/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/stylesheet/atoms/button/button.css"/>
+    <link rel="stylesheet" href="../public/stylesheet/atoms/input/input.css"/>
+    <link rel="stylesheet" href="../public/stylesheet/general/general.css">
+    <link rel="stylesheet" href="../public/stylesheet/atoms/box/box.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="box box--bg-red">
+        <span class="box__desc">
+            Box one
+        </span>
+    </div>
+
+    <!-- <button class="button--bg-blue button button--size-md button--pill button--pressed-effect">
+        <a href="" class="button__link button__link--fr-white">
+            <span type="" class="button__desc button__desc--fr-white button__desc--uppercase">Click</span>
+        </a>
+    </button>
+
+
+    <input type="text" class="input input--round input--shadow input--size-md"> -->
+</body>
+
+</html>
